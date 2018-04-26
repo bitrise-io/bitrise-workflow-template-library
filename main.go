06f5ba7a5b7b36b609e232f0a407b855139a030a
Papik Tamas <trapacska@gmail.com>
2018-04-26 14:29:39 +0200

    PR changes

--- main.go
+++ main.go
@@ -10,18 +10,19 @@ import (
 	"path/filepath"
 	"strings"
 
+	stepmanModels "github.com/bitrise-io/stepman/models"
 	"gopkg.in/yaml.v2"
 )
 
 const outputDir = "./_tmp"
 const collectionDir = "./templates"
-const specFileName = "template-spec.json"
+const templateSpecFileName = "template-spec.json"
 const templateFileName = "template.yml"
 const steplibSpecJSONURI = "https://bitrise-steplib-collection.s3.amazonaws.com/spec.json"
 
 type step struct {
-	Description string      `json:"description"`
-	Info        interface{} `json:"step_info"`
+	Description string                  `json:"description"`
+	Info        stepmanModels.StepModel `json:"step_info"`
 }
 
 type template struct {
@@ -30,44 +31,47 @@ type template struct {
 	Summary     string           `json:"summary"`
 	Description string           `json:"description"`
 	Image       string           `json:"image"`
-	Yaml        string           `json:"yaml"`
+	Config      string           `json:"config"`
 	Steps       map[string]*step `json:"steps"`
 }
 
-type steplibSpec struct {
-	Steps map[string]struct {
-		LatestVersion string                 `json:"latest_version_number"`
-		Versions      map[string]interface{} `json:"versions"`
+func stepParams(step string) (stepID string, stepVersion string) {
+	stepID = step
+	if s := strings.Split(step, "@"); len(s) > 1 {
+		stepID = s[0]
+		stepVersion = s[1]
 	}
+	return
 }
 
-func (slibSpec steplibSpec) fetchVersion(stepID string) (interface{}, error) {
-	s := strings.Split(stepID, "@")
-
-	step, ok := slibSpec.Steps[s[0]]
-	if !ok {
-		return nil, fmt.Errorf("no step found with id: %s", s[0])
+func parseTemplate(templateSpec map[string]*template, templateID string) error {
+	ymlFile, err := os.Open(getYMLPath(templateID))
+	if err != nil {
+		return err
 	}
+	templateSpec[templateID] = &template{}
+	return yaml.NewDecoder(ymlFile).Decode(templateSpec[templateID])
+}
 
-	if len(s) == 2 {
-		stepInfo, ok := step.Versions[s[1]]
-		if !ok {
-			return nil, fmt.Errorf("no version(%s) found for stepID: %s", s[1], s[0])
-		}
-		return stepInfo, nil
+func getYMLPath(templateID string) string {
+	return filepath.Join(collectionDir, templateID, templateFileName)
+}
+
+func getSpecJSON() (steplibSpec stepmanModels.StepCollectionModel, err error) {
+	resp, err := http.Get(steplibSpecJSONURI)
+	if err != nil {
+		return
 	}
-
-	v, ok := step.Versions[step.LatestVersion]
-	if !ok {
-		return nil, fmt.Errorf("no version(%s) found for stepID: %s", step.LatestVersion, s[0])
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return stepmanModels.StepCollectionModel{}, fmt.Errorf("nonsuccessful response statusCode: %s", steplibSpecJSONURI)
 	}
-
-	return v, nil
+	err = json.NewDecoder(resp.Body).Decode(&steplibSpec)
+	return
 }
 
 func main() {
 	//get steplib spec json
-	slibSpec, err := getSpecJSON()
+	steplibSpec, err := getSpecJSON()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -78,29 +82,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// populate tplSpec | templateID: templateData
-	tplSpec := map[string]*template{}
+	// populate templateSpec | templateID: templateData
+	templateSpec := map[string]*template{}
 	for _, file := range files {
 		// parse the template
-		err := parseTemplate(tplSpec, file.Name())
+		err := parseTemplate(templateSpec, file.Name())
 		if err != nil {
 			log.Fatal(err)
 		}
 		// filling step infos from spec json
-		for stepID := range tplSpec[file.Name()].Steps {
-			i, err := slibSpec.fetchVersion(stepID)
+		for s := range templateSpec[file.Name()].Steps {
+			i, _, _ := steplibSpec.GetStepVersion(stepParams(s))
 			if err != nil {
 				log.Fatal(err)
 			}
-			if tplSpec[file.Name()].Steps[stepID] == nil {
-				tplSpec[file.Name()].Steps[stepID] = &step{}
+			if templateSpec[file.Name()].Steps[s] == nil {
+				templateSpec[file.Name()].Steps[s] = &step{}
 			}
-			tplSpec[file.Name()].Steps[stepID].Info = i
+			templateSpec[file.Name()].Steps[s].Info = i.Step
 		}
 		fmt.Println("<-", file.Name())
 	}
 
-	out, err := os.Create(filepath.Join(outputDir, specFileName))
+	out, err := os.Create(filepath.Join(outputDir, templateSpecFileName))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -110,36 +114,7 @@ func main() {
 		}
 	}()
 
-	err = json.NewEncoder(out).Encode(tplSpec)
-	if err != nil {
+	if err := json.NewEncoder(out).Encode(templateSpec); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func parseTemplate(tplSpec map[string]*template, templateID string) error {
-	ymlFile, err := os.Open(getYMLPath(templateID))
-	if err != nil {
-		return err
-	}
-	tplSpec[templateID] = &template{}
-	return yaml.NewDecoder(ymlFile).Decode(tplSpec[templateID])
-}
-
-func getYMLPath(templateID string) string {
-	return filepath.Join(collectionDir, templateID, templateFileName)
-}
-
-func getSpecJSON() (slibSpec steplibSpec, err error) {
-	resp, err := http.Get(steplibSpecJSONURI)
-	if err != nil {
-		return
-	}
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return steplibSpec{}, fmt.Errorf("non-successful response statusCode: %s", steplibSpecJSONURI)
-	}
-	err = json.NewDecoder(resp.Body).Decode(&slibSpec)
-	if err != nil {
-		return
-	}
-	return
 }
